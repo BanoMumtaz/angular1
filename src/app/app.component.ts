3e8b8b9a1423fb033fb95216f33c23c7749d84d2
Mumtaz Bano <mumtaz1995bano@gmail.com>
2023-10-05 02:30:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
 })
-export class AppComponent {
-  title = 'angular1';
+export class ContactFormComponent {
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_qbeq1id', 'template_d37bkw6', e.target as HTMLFormElement, 'GbaQxc_ppEIbcc2h8')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        alert('Email sent successfully!');
+        // Reset the form
+        (e.target as HTMLFormElement).reset();
+      }, (error) => {
+        console.log(error.text);
+        alert('Error sending email: ' + error.text);
+      });
+  }
 }
+
